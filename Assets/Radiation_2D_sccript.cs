d544344c921795a38a480e40ac3d13cb58b9f142
Jiriceknvm <j.trojek.st@spseiostrava.cz>
2025-10-24 12:28:37 +0200

    radiace_zaklad

---
+++ Assets/Radiation_2D_sccript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Radiation_2D_sccript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
