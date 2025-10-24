eb893d2ef1ea830aaa1d4709a9bff3a0238ddf41
Fajkl158 <radekkokor158@seznam.cz>
2025-10-24 18:14:26 +0200

    final hrac animace

--- Assets/Playermovement03.cs
+++ Assets/Playermovement03.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 
-
 public class Playermovement03 : MonoBehaviour
 {
     public float moveSpeed = 5f;
@@ -8,6 +7,8 @@ public class Playermovement03 : MonoBehaviour
 
     private Rigidbody2D rb;
     private bool isGrounded = true;
+
+    public Animator anim;
 
     private void Start()
     {
@@ -18,24 +19,50 @@ public class Playermovement03 : MonoBehaviour
     {
         // Pohyb doleva/doprava
         float moveX = 0f;
-        if (Input.GetKey(KeyCode.A))
-            moveX = -1f;
-        if (Input.GetKey(KeyCode.D))
-            moveX = 1f;
+        if (Input.GetKey(KeyCode.A)) moveX = -1f;
+        if (Input.GetKey(KeyCode.D)) moveX = 1f;
 
         rb.linearVelocity = new Vector2(moveX * moveSpeed, rb.linearVelocity.y);
+
+        // Animace běhu
+        anim.SetBool("isRunning", moveX != 0);
 
         // Skok
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             isGrounded = false;
+            anim.SetBool("JumpStart", true);  // první fáze skoku
+        }
+
+        // Kontrola stavu ve vzduchu
+        if (!isGrounded)
+        {
+            float yVel = rb.linearVelocity.y;
+
+            if (yVel > 0.1f)
+            {
+                anim.SetBool("JumpUp", true);
+                anim.SetBool("JumpDown", false);
+            }
+            else if (yVel < -0.1f)
+            {
+                anim.SetBool("JumpUp", false);
+                anim.SetBool("JumpDown", true);
+            }
+        }
+        else
+        {
+            // Reset po dopadu
+            anim.SetBool("JumpStart", false);
+            anim.SetBool("JumpUp", false);
+            anim.SetBool("JumpDown", false);
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // Když dopadne na zem, může znovu skákat
-        isGrounded = true;
+        if (collision.contacts[0].normal.y > 0.5f)
+            isGrounded = true;
     }
 }
